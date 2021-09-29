d90bd6ad3d364f89f3fb116b2ddce54b9eaa2347
AntonioTach <antach.vihe@gmail.com>
2021-09-28 21:39:28 -0500

    Configuraciones básicas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 
 
 app.set('port', process.env.port || 4000);
+// app.use(cors({origin: "http://localhost:4200"})); //Recibir Peticiones exclusivamente de angular
+app.use(cors());
 
 app.use(morgan('dev'));
 
