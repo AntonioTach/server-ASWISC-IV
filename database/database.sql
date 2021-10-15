d60cd4d662b5fcd6d2e0fac9e23285452d946e9a
leonardo20002 <lrodriguezp0522@gmail.com>
2021-10-15 18:44:42 -0500

    listo
    nueva tabla en bd, rutas y controler de paciente

--- database/database.sql
+++ database/database.sql
@@ -23,3 +23,21 @@ ALTER TABLE especialistas
     MODIFY id_especialista INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
 
 DESCRIBE especialistas;
+
+CREATE TABLE pacientes{
+    id_paciente INT(11) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    usuario VARCHAR(60) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    contraseña VARCHAR(50) NOT NULL,
+    sexo VARCHAR(10) NOT NULL,
+    telefono BIGINT NOT NULL,
+    nombretutor VARCHAR(100) NOT NULL,
+    telefonotutor BIGINT NOT NULL
+}
+
+ALTER TABLE pacientes
+    ADD PRIMARY KEY (id_paciente);
+
+ALTER TABLE pacientes
+    MODIFY id_paciente INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
