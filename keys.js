6ef9787ba5d635a3ad1324ce2c69acc870eaf00a
leonardo20002 <lrodriguezp0522@gmail.com>
2021-10-31 18:28:20 -0600

    cambiale el puerto
    Cambiale el puerto

--- keys.js
+++ keys.js
@@ -5,6 +5,6 @@ module.exports = {
         user: 'root',
         password: '',
         database: 'database_aswisc',
-        port: 3310,
+        port: 4000,
     }
 }
---
+++ src/controllers/auth.js
@@ -0,0 +1,47 @@
+const { validationResult } = require('express-validator');
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+
+
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const user = await User.find(email);
+
+    if (user[0].length !== 1) {
+      const error = new Error('A user with this email could not be found.');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const storedUser = user[0][0];
+
+    const isEqual = await bcrypt.compare(password, storedUser.password);
+
+    if (!isEqual) {
+      const error = new Error('Contraseña incorrecta');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      {
+        email: storedUser.email,
+        userId: storedUser.id,
+      },
+      'secretfortoken',
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ token: token, userId: storedUser.id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
