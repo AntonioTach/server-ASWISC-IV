ae016132cb38ee5bd3dfa96371a188e450d4b264
leonardo20002 <lrodriguezp0522@gmail.com>
2022-01-28 00:04:59 -0600

    Update aswisc.controller.js

--- src/controllers/aswisc.controller.js
+++ src/controllers/aswisc.controller.js
@@ -6,8 +6,10 @@ aswiscCtrl.automatizarPrueba =  async (req, res) => {
 
     const { id, nombre, fecha, cubos, semejanzas, digitos, conceptos, claves, vocabulario, letras, matrices, comprension, 
     busqueda, figuras, animales, informacion, aritmetica, adivinanza } = req.body;
+    console.log(req.body);
     let sql = ` SELECT FROM pacientes where id_paciente = ${id}`;
     await pool.query(sql); 
+    
  
 } 
 
