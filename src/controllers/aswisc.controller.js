b4616a2d9f2cf4172aa9d03ebef2da30f36b2beb
AntonioTach <antach.vihe@gmail.com>
2022-01-28 00:22:37 -0600

    Update aswisc.controller.js

--- src/controllers/aswisc.controller.js
+++ src/controllers/aswisc.controller.js
@@ -1,6 +1,8 @@
 const aswiscCtrl = {};
 
 const e = require('express');
+const pool = require('../database');
+const res = require('express/lib/response');
 //----------------------Solucion Automatizada WISC-IV "ASWISC-IV"----------------------------
 aswiscCtrl.automatizarPrueba = (req, res) => {
 
