1203b164f50648887ce24246e425640f92911e87
leonardo20002 <lrodriguezp0522@gmail.com>
2022-01-27 23:58:00 -0600

    inicio básico

--- src/controllers/aswisc.controller.js
+++ src/controllers/aswisc.controller.js
@@ -2,8 +2,13 @@ const aswiscCtrl = {};
 
 const e = require('express');
 //----------------------Solucion Automatizada WISC-IV "ASWISC-IV"----------------------------
-aswiscCtrl.automatizarPrueba = (req, res) => {
+aswiscCtrl.automatizarPrueba =  async (req, res) => {
 
-}
+    const { id, nombre, fecha, cubos, semejanzas, digitos, conceptos, claves, vocabulario, letras, matrices, comprension, 
+    busqueda, figuras, animales, informacion, aritmetica, adivinanza } = req.body;
+    let sql = ` SELECT FROM pacientes where id_paciente = ${id}`;
+    await pool.query(sql); 
+ 
+} 
 
-module.exports = aswiscCtrl;
+module.exports = aswiscCtrl; 
