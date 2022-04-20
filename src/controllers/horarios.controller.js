9f8301ebcd7c9b720cff42d94e5ca97d4a11bdf2
AntonioTach <antach.vihe@gmail.com>
2022-04-19 22:14:00 -0500

    token y creacion evento

--- src/controllers/horarios.controller.js
+++ src/controllers/horarios.controller.js
@@ -4,19 +4,39 @@ const { google } = required('googleapis');
 
 const { OAuth2 } = google.auth;
 
-const oAuth2Client = new OAuth2('199421933679-9jhp14gkvnk5plslrlbcl573lvp0s7b6.apps.googleusercontent.com', 
-'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'
+const oAuth2Client = new OAuth2('57432841693-mijbi1j1o5fmo3vm7t2jpuudjrdqkcov.apps.googleusercontent.com', 
+'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'
 )
 
 oAuth2Client.setCredentials({
     refresh_token: 
-    '1//044rxikaQEfFkCgYIARAAGAQSNwF-L9IrfzQ6gLiyQN3y5-XU2UN4RGDfdBivG935FYJYuS82gLYXZvdrJamjng-Nl3pSC1OShKs',
+    '1//046DzW408YyPZCgYIARAAGAQSNgF-L9IrREZKofmb9BbVXRuWaKK-cyg1kM0ibowQv310kGogtEXZAfVkE7NfN77xSXLxfbpxcw',
 });
 
 const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 
 horariosCtrl.generarVideollamada = async (req, res) => {
+    const eventStartTime = new Date();
+    eventStartTime.setDate(eventStartTime.getDate() + 2); //Tomorrow 
 
+    const eventEndTime = new Date();
+    eventEndTime.setDate(eventStartTime.getDate() + 2); 
+    eventEndTime.setDate(eventEndTime.getDate() + 60); //60 minutes later
+
+    //Configuration 
+    const event = {
+        sumary : 'Meeting', 
+        description: 'Test Meet',
+        start : {
+            dateTime : eventStartTime,
+            timeZone: 'America/Mexico_City',
+        },
+        end : {
+            dateTime: eventEndTime,
+            timeZone: 'America/Mexico_City',
+        },
+        colorId: 1
+    };
 }
 
 module.exports = horariosCtrl;
